fdca0f4c0290a2f3a5e82e9098f3f692ff7f5623
Aline Iasmim <aline.isilvadamiao@gmail.com>
2021-05-23 22:19:19 -0300

    Add files via upload

---
+++ src/pages/Dashboard/Sobre.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Sobre</h1>
+    <q-btn
+      round
+      color="primary"
+      class="fixed"
+      style="right: 18px; bottom: 18px"
+      @click="abrirCadastroSobre()"
+    >
+      <q-icon name="add" />
+    </q-btn> 
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageSobre',
+  methods: {
+    abrirCadastroSobre () {
+      this.$router.push('/adsobre')
+    }
+  }
+}
+</script>
